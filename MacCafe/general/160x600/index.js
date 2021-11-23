f8b9ceb6672628267a3d69db56534d0481e19a7d
Lev Shushkov <levashushkov@yandex.ru>
2021-11-23 09:37:42 +0300

    office + general

---
+++ MacCafe/general/160x600/index.js
@@ -0,0 +1,102 @@
+const start = document.querySelector(".first-iteration");
+const texts = document.querySelector(".banner__texts");
+const firstTitles = document.querySelector(".title__first");
+const secondTitles = document.querySelector(".title__second");
+const cup = document.querySelector(".banner__cup");
+const logo = document.querySelector(".banner__logo");
+const phone = document.querySelector(".banner__phone");
+const price = document.querySelector(".banner__price");
+const button = document.querySelector(".banner__button");
+const end = document.querySelector(".banner__end");
+
+const firstStroke = document.querySelector(".title__stroke_one");
+const secondStroke = document.querySelector(".title__stroke_two");
+const thirdStroke = document.querySelector(".title__stroke_three");
+
+let clickout;
+let clickEvent;
+
+const now = new Date();
+const hour = now.getHours();
+
+animate();
+changeTexts();
+checkSize()
+
+function changeTexts() {
+    if (hour >= 0 && hour < 12) {
+        firstStroke.textContent = "Начните";
+        secondStroke.textContent = "день";
+        thirdStroke.textContent = "c МакКафе";
+        clickout = "default";
+        screenad.event("IMPRESSION_1");
+        clickEvent = "CLICK_1";
+    } else if (hour >= 12 && hour < 15) {
+        firstStroke.textContent = "Пообедайте";
+        secondStroke.textContent = "с МакКафе";
+        clickout = "extra1";
+        screenad.event("IMPRESSION_2");
+        clickEvent = "CLICK_2";
+    } else if (hour >= 15 && hour < 18) {
+        firstStroke.textContent = "Сделайте";
+        secondStroke.textContent = "перерыв";
+        thirdStroke.textContent = "c МакКафе";
+        clickout = "extra2";
+        screenad.event("IMPRESSION_3");
+        clickEvent = "CLICK_3";
+    } else if (hour >= 18 && hour < 24) {
+        firstStroke.textContent = "Начните";
+        secondStroke.textContent = "день";
+        thirdStroke.textContent = "c МакКафе";
+        clickout = "default";
+        screenad.event("IMPRESSION_1");
+        clickEvent = "CLICK_1";
+    }
+}
+
+function checkSize() {
+    if (document.querySelector(".title__first").width > 140) {
+        let fSize = 12;
+        document.querySelector(".title__stroke_one").style.fontSize = fSize + "px";
+        document.querySelector(".title__stroke_two").style.fontSize = fSize + "px";
+        document.querySelector(".title__stroke_three").style.fontSize = fSize + "px";
+    }
+}
+
+function animate() {
+    end.classList.remove("end__anim");
+    setTimeout(function () {
+        texts.classList.add("title__anim");
+        cup.classList.add("cup__anim");
+        logo.classList.add("logo__anim");
+        secondTitles.style.opacity = "0";
+        phone.classList.add("phone__anim");
+
+        setTimeout(function () {
+            button.classList.add("button__anim");
+            secondTitles.style.opacity = "1";
+            firstTitles.style.opacity = "0";
+            price.classList.add("price__anim");
+            texts.classList.remove("title__anim");
+            texts.classList.add("title__anim_second");
+
+            setTimeout(function () {
+                end.classList.add("end__anim");
+                setTimeout(function () {
+                    phone.classList.remove("phone__anim");
+                    texts.classList.remove("title__anim");
+                    cup.classList.remove("cup__anim");
+                    logo.classList.remove("logo__anim");
+                    button.classList.remove("button__anim");
+                    price.classList.remove("price__anim");
+                    texts.classList.remove("title__anim_second");
+                    secondTitles.style.opacity = "0";
+                    firstTitles.style.opacity = "1";
+                    setTimeout(function () {
+                        animate();
+                    }, 1500);
+                }, 500);
+            }, 4000);
+        }, 4000);
+    }, 0);
+}
