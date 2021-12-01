ad540d264d80d15259bb1b28c3108fc2715390f6
Lev Shushkov <levashushkov@yandex.ru>
2021-12-01 19:55:23 +0300

    VTB24 7-th screen

--- portfolio/VTB24/index.js
+++ portfolio/VTB24/index.js
@@ -1,9 +1,15 @@
 const banner = document.querySelector(".banner");
-const handContainer = document.querySelector(".hand-container");
 const firstScreen = document.querySelector(".first-screen");
 const secondScreen = document.querySelector(".second-screen");
 const thirdScreen = document.querySelector(".third-screen");
 const fourthScreen = document.querySelector(".fourth-screen");
+const fifthScreen = document.querySelector(".fifth-screen");
+const sixthScreen = document.querySelector(".sixth-screen");
+const seventhScreen = document.querySelector(".seventh-screen");
+const disclamerScreenOne = document.querySelector(".disclamer-one");
+const disclamerScreenTwo = document.querySelector(".disclamer-two");
+const percent = document.querySelector(".percent");
+
 const hand = document.querySelector(".hand");
 const card = document.querySelector(".card");
 const transition = document.querySelector(".transition");
@@ -11,17 +17,27 @@ const chair = document.querySelector(".chair");
 const cart = document.querySelector(".cart");
 const vtbCard = document.querySelector(".vtb-card");
 const coctail = document.querySelector(".coctail");
+const aircraft = document.querySelector(".aircraft");
 
 let clickCounter = 0;
 
 banner.addEventListener("mousemove", moveOnMouse);
-banner.addEventListener("mousemove", handMove);
+banner.addEventListener("mousemove", handMoveX);
+banner.addEventListener("mousemove", handMoveY);
 banner.addEventListener("mouseout", handDefault);
 banner.addEventListener("mouseout", moveToDefault);
+firstScreen.addEventListener("click", changeAnim);
+secondScreen.addEventListener("click", moveChair);
+thirdScreen.addEventListener("click", moveCart);
+fourthScreen.addEventListener("click", moveCard);
+fifthScreen.addEventListener("click", fiveScreen);
+sixthScreen.addEventListener("click", movePercent);
 
-function handMove(event) {
-    hand.style.top = event.pageY - 100 + "px";
-    hand.style.left = event.pageX - 170 + "px";
+function handMoveX(event) {
+    hand.style.left = event.pageX - 350 + "px";
+}
+function handMoveY(event) {
+    hand.style.top = event.pageY - 240 + "px";
 }
 
 function handDefault() {
@@ -31,10 +47,6 @@ function handDefault() {
 function moveOnMouse() {
     hand.classList.remove("hand-default");
     card.classList.remove("card-anim-deactive");
-    firstScreen.addEventListener("click", changeAnim);
-    secondScreen.addEventListener("click", moveChair);
-    thirdScreen.addEventListener("click", moveCart);
-    fourthScreen.addEventListener("click", moveCard);
 }
 
 function changeAnim() {
@@ -50,10 +62,10 @@ function changeAnim() {
             firstScreen.style.display = "none";
             secondScreen.style.display = "block";
             moveToDefault();
-        }, 1000);
+        }, 500);
         setTimeout(function () {
             transition.classList.remove("transition-anim");
-        }, 2000);
+        }, 550);
     }
 }
 
@@ -81,10 +93,10 @@ function moveChair() {
             secondScreen.style.display = "none";
             thirdScreen.style.display = "block";
             moveToDefault();
-        }, 1000);
+        }, 500);
         setTimeout(function () {
             transition.classList.remove("transition-anim");
-        }, 2000);
+        }, 500);
     }
 }
 
@@ -102,15 +114,17 @@ function moveCart() {
             cart.classList.remove("cart-anim-two");
         }, 1500);
         clickCounter = 0;
-        transition.classList.add("transition-anim");
         setTimeout(function () {
-            thirdScreen.style.display = "none";
-            fourthScreen.style.display = "block";
-            moveToDefault();
+            transition.classList.add("transition-anim");
             setTimeout(function () {
-                transition.classList.remove("transition-anim");
-            }, 2000);
-        }, 1000);
+                thirdScreen.style.display = "none";
+                fourthScreen.style.display = "block";
+                moveToDefault();
+                setTimeout(function () {
+                    transition.classList.remove("transition-anim");
+                }, 500);
+            }, 500);
+        }, 500);
     }
 }
 
@@ -126,13 +140,62 @@ function moveCard() {
         setTimeout(function () {
             fourthScreen.style.display = "none";
             fifthScreen.style.display = "block";
-            moveToDefault();
-
             setTimeout(function () {
                 vtbCard.classList.remove("anim-to-transparency");
                 coctail.classList.remove("anim-to-visibility");
                 transition.classList.remove("transition-anim");
-            }, 2000);
+            }, 500);
+        }, 500);
+    }
+}
+
+function fiveScreen() {
+    clickCounter++;
+
+    aircraft.classList.add("aircraft-anim");
+
+    setTimeout(function () {
+        aircraft.classList.remove("aircraft-anim");
+    }, 3000);
+
+    if (clickCounter === 2) {
+        aircraft.classList.add("aircraft-anim-two");
+        setTimeout(function () {
+            aircraft.classList.remove("aircraft-anim-two");
         }, 1000);
+        clickCounter = 0;
+        setTimeout(function () {
+            transition.classList.add("transition-anim");
+            setTimeout(function () {
+                fifthScreen.style.display = "none";
+                sixthScreen.style.display = "block";
+                setTimeout(function () {
+                    transition.classList.remove("transition-anim");
+                }, 500);
+            }, 500);
+        }, 500);
+    }
+}
+
+function movePercent() {
+    clickCounter++;
+
+    percent.classList.add("percent-anim");
+    setTimeout(function () {
+        percent.classList.remove("percent-anim");
+    }, 2000);
+
+    if (clickCounter === 2) {
+        clickCounter = 0;
+        transition.classList.add("transition-anim");
+        setTimeout(function () {
+            sixthScreen.style.display = "none";
+            seventhScreen.style.display = "block";
+            setTimeout(function () {
+                percent.classList.remove("percent-anim");
+
+                transition.classList.remove("transition-anim");
+            }, 500);
+        }, 500);
     }
 }
