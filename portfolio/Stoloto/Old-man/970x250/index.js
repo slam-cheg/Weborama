728866afbb6963b19c673cb4907a42e3e1f7a7ff
Lev Shushkov <levashushkov@yandex.ru>
2022-02-04 18:40:00 +0300

    loto 6/8

---
+++ portfolio/Stoloto/Old-man/970x250/index.js
@@ -0,0 +1,34 @@
+const banner = document.querySelector(".banner");
+const star = document.querySelector(".star");
+const button = document.querySelector(".button");
+const wrapper = document.querySelector(".wrapper");
+const lights = document.querySelector(".auto-lights");
+
+button.addEventListener("click", function () {
+    screenad.click();
+});
+banner.addEventListener("click", function () {
+    screenad.click();
+});
+
+animation();
+
+function animation() {
+    lights.classList.add("auto-lights-anim");
+    setTimeout(function () {
+        wrapper.classList.add("screen-two");
+        star.classList.add("star-rotate");
+        lights.classList.remove("auto-lights-anim");
+        setTimeout(function () {
+            star.classList.remove("star-rotate");
+            wrapper.classList.add("screen-three");
+            setTimeout(function () {
+                wrapper.classList.remove("screen-two");
+                wrapper.classList.remove("screen-three");
+                setTimeout(function () {
+                    animation();
+                }, 500);
+            }, 4000);
+        }, 4000);
+    }, 4000);
+}
