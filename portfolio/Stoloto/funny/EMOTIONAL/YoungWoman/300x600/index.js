c33ef2c0ea66fd7e8c90ca3926c899b0f37738aa
Lev Shushkov <levashushkov@yandex.ru>
2022-02-16 17:41:12 +0300

    stoloto ready

---
+++ portfolio/Stoloto/funny/EMOTIONAL/YoungWoman/300x600/index.js
@@ -0,0 +1,77 @@
+const banner = document.querySelector(".banner");
+const button = document.querySelector(".button");
+const wrapper = document.querySelector(".wrapper");
+let customEvent = "DEFAULT";
+let customClick = `DEFAULT_CLICK`;
+
+button.addEventListener("click", function () {
+    screenad.click();
+    screenad.event(`${customClick}`);
+});
+banner.addEventListener("click", function () {
+    screenad.click();
+    screenad.event(`${customClick}`);
+});
+
+animation();
+
+function animation() {
+    setTimeout(function () {
+        wrapper.classList.add("screen-one");
+        setTimeout(function () {
+            wrapper.classList.add("screen-two");
+            setTimeout(function () {
+                setTimeout(function () {
+                    wrapper.classList.add("screen-three");
+                    setTimeout(function () {
+                        wrapper.classList.remove("screen-three");
+                        wrapper.classList.remove("screen-two");
+                        wrapper.classList.remove("screen-one");
+                        setTimeout(function () {
+                            animation();
+                        }, 500);
+                    }, 4000);
+                }, 3000);
+            }, 1000);
+        }, 4000);
+    }, 500);
+}
+
+const titleWrapper = document.querySelector(".title-wrapper");
+const titleLineOne = document.querySelector(".title-line-one");
+const titleLineTwo = document.querySelector(".title-line-two");
+const titleLineThree = document.querySelector(".title-line-three");
+
+const words = [
+    ["Пена и\u00A0носки", "уже\u00A0не\u00A0то?", 'Подарите ему билет "Русское\u00A0лото"!'],
+    ["Хотите машину, в\u00A0отпуск", "и\u00A0много всего?", 'Пора играть в\u00A0"Русское\u00A0лото"!'],
+    ["Праздничное утро начинается", 'с\u00A0билета "Русское\u00A0лото"!'],
+];
+rotation();
+function rotation() {
+    let randomArr = words[Math.floor(Math.random() * words.length)];
+    titleWrapper.style.lineHeight = "1.1";
+    if (randomArr === words[0]) {
+        titleWrapper.style.fontSize = "22px";
+        titleWrapper.style.top = "90px";
+        customEvent = "FUNNY_ONE";
+        screenad.event(`${customEvent}`);
+        customClick = `${customEvent}_CLICK`;
+    }
+    if (randomArr === words[1]) {
+        titleWrapper.style.fontSize = "17px";
+        customEvent = "FUNNY_TWO";
+        screenad.event(`${customEvent}`);
+        customClick = `${customEvent}_CLICK`;
+    }
+    if (randomArr === words[2]) {
+        titleWrapper.style.fontSize = "22px";
+        titleWrapper.style.top = "90px";
+        customEvent = "FUNNY_THREE";
+        screenad.event(`${customEvent}`);
+        customClick = `${customEvent}_CLICK`;
+    }
+    titleLineOne.textContent = randomArr[0];
+    titleLineTwo.textContent = randomArr[1];
+    titleLineThree.textContent = randomArr[2];
+}
