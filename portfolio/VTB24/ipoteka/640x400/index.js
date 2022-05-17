ff511947e0c25c4239eac3e925479b68c0dfde06
Lev Shushkov <levashushkov@yandex.ru>
2022-05-17 16:33:41 +0300

    640x400 ready + GAO

---
+++ portfolio/VTB24/ipoteka/640x400/index.js
@@ -0,0 +1,80 @@
+const banner = document.querySelector(".banner");
+const minPayment = banner.querySelector(".description__minpay");
+const plate = banner.querySelector(".plate");
+const percent = banner.querySelector(".percent");
+const title = banner.querySelector(".title");
+const description = banner.querySelectorAll(".description");
+const slash = banner.querySelector(".slash");
+const button = banner.querySelector(".button");
+const disclamerText = banner.querySelector(".disclamer__text");
+const tel = banner.querySelector(".tel");
+
+let customEvent = "DEFAULT";
+let customClick = `DEFAULT_CLICK`;
+
+banner.addEventListener("click", function () {
+    screenad.click("default");
+});
+
+// GEO
+navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = Math.round(position.coords.latitude);
+    const longitude = Math.round(position.coords.longitude);
+    getCsv(latitude, longitude);
+});
+
+// JSON Data Cities
+function getCsv(latitude, longitude) {
+    return fetch("./cities.json", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    })
+        .then(checkResponse)
+        .then((res) => {
+            for (let i = 0; i < res.length; i++) {
+                
+                if (Math.abs(latitude - res[i].latitude) < 2 && Math.abs(longitude - res[i].longitude) < 2) { // region check
+                    if (Math.abs(latitude - res[i].latitude) < 0.5 && Math.abs(longitude - res[i].longitude) < 0.5) { // city check
+                        minPayment.textContent = `${res[i].min_payment}`;
+                        customEvent = res[i].event;
+                        customClick = `${customEvent}_CLICK`;
+                        screenad.event(customEvent);
+                        break;
+                    }
+                    minPayment.textContent = `${res[i].min_payment}`;
+                    customEvent = res[i].event;
+                    customClick = `${customEvent}_CLICK`;
+                    screenad.event(customEvent);
+                    break;
+                }
+            }
+        })
+        .catch((err) => {
+            console.warn(err);
+        });
+}
+
+const checkResponse = (res) => {
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+};
+
+setTimeout(function () {
+    plate.style.transform = "translateX(0)";
+    percent.style.transform = "scale(1)";
+    setTimeout(function () {
+        title.style.opacity = "1";
+        description.forEach((p) => {
+            p.style.opacity = "1";
+        });
+        slash.style.opacity = "1";
+        button.style.transform = "translateX(0)";
+        tel.style.transform = "translateX(0)";
+        setTimeout(function () {
+            percent.classList.add("scale-anim");
+            disclamerText.style.transform = "translateY(0)";
+        }, 500);
+    }, 500);
+}, 1000);
