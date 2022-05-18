b20c92d8cfc081890f29348ad93d622d6f509b3b
Lev Shushkov <levashushkov@yandex.ru>
2022-05-18 11:42:47 +0300

    pure

--- portfolio/VTB24/ipoteka/pure/640x400/index.js
+++ portfolio/VTB24/ipoteka/pure/640x400/index.js
@@ -3,17 +3,20 @@ const minPayment = banner.querySelector(".description__minpay");
 const plate = banner.querySelector(".plate");
 const percent = banner.querySelector(".percent");
 const title = banner.querySelector(".title");
-const description = banner.querySelectorAll(".description");
+const description = banner.querySelector(".description");
 const slash = banner.querySelector(".slash");
 const button = banner.querySelector(".button");
 const disclamerText = banner.querySelector(".disclamer__text");
+const disclamerChange = banner.querySelector(".disclamer__change");
 const tel = banner.querySelector(".tel");
 
 let customEvent = "DEFAULT";
 let customClick = `DEFAULT_CLICK`;
 
 banner.addEventListener("click", function () {
-    screenad.click("default");
+    screenad.click();
+    screenad.event(`${customClick}`);
+    console.log(`Click event: ${customClick}, Event: ${customEvent}`);
 });
 
 // GEO
@@ -32,16 +35,19 @@ function getCsv(latitude, longitude) {
         .then(checkResponse)
         .then((res) => {
             for (let i = 0; i < res.length; i++) {
-                
-                if (Math.abs(latitude - res[i].latitude) < 2 && Math.abs(longitude - res[i].longitude) < 2) { // region check
-                    if (Math.abs(latitude - res[i].latitude) < 0.5 && Math.abs(longitude - res[i].longitude) < 0.5) { // city check
+                // region check
+                if (Math.abs(latitude - res[i].latitude) < 2 && Math.abs(longitude - res[i].longitude) < 2) {
+                    // city check
+                    if (Math.abs(latitude - res[i].latitude) < 0.5 && Math.abs(longitude - res[i].longitude) < 0.5) {
                         minPayment.textContent = `${res[i].min_payment}`;
+                        disclamerChange.textContent = `${res[i].legal}`;
                         customEvent = res[i].event;
                         customClick = `${customEvent}_CLICK`;
                         screenad.event(customEvent);
                         break;
                     }
                     minPayment.textContent = `${res[i].min_payment}`;
+                    disclamerChange.textContent = `${res[i].legal}`;
                     customEvent = res[i].event;
                     customClick = `${customEvent}_CLICK`;
                     screenad.event(customEvent);
@@ -66,10 +72,8 @@ setTimeout(function () {
     percent.style.transform = "scale(1)";
     setTimeout(function () {
         title.style.opacity = "1";
-        description.forEach((p) => {
-            p.style.opacity = "1";
-        });
-        slash.style.opacity = "1";
+        description.style.opacity = "1";
+        // slash.style.opacity = "1";
         button.style.transform = "translateX(0)";
         tel.style.transform = "translateX(0)";
         setTimeout(function () {
